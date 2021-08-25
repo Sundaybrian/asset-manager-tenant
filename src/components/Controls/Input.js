6e60e4875919b1ea5c1a2b4fbf2f124f37ab6f12
Brian Sunday <brians931@gmail.com>
2021-08-25 15:57:43 +0300

    add: control components

--- src/components/Controls/Input.js
+++ src/components/Controls/Input.js
@@ -1,18 +1,9 @@
-import React from "react";
-import { TextField } from "@material-ui/core";
+import React from 'react';
+import { TextField } from '@material-ui/core';
 
 function Input(props) {
-    const { name, value, onChange, label, ...rest } = props;
-    return (
-        <TextField
-            variant="outlined"
-            name={name}
-            value={value}
-            onChange={onChange}
-            label={label}
-            {...rest}
-        />
-    );
+  const { name, value, onChange, label, ...rest } = props;
+  return <TextField variant="outlined" name={name} value={value} onChange={onChange} label={label} {...rest} />;
 }
 
 export default Input;
