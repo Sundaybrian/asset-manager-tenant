ad104d84d8c3c3dc56345510824724662b5d3e31
Brian Sunday <brians931@gmail.com>
2021-08-25 16:00:04 +0300

    add: content component

--- src/components/Container/Content.js
+++ src/components/Container/Content.js
@@ -1,49 +1,48 @@
-import React from "react";
-import Container from "@material-ui/core/Container";
-
-import { makeStyles } from "@material-ui/core/styles";
+import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
+import React from 'react';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        display: "flex",
-    },
-    content: {
-        flexGrow: 1,
-        overflow: "auto",
-        padding: theme.spacing(2),
+  root: {
+    display: 'flex',
+  },
+  content: {
+    flexGrow: 1,
+    overflow: 'auto',
+    padding: theme.spacing(2),
 
-        // [theme.breakpoints.up("sm")]: {
-        //     width: `calc(100% - ${drawerWidth}px)`,
-        //     marginLeft: drawerWidth,
-        // },
-    },
+    // [theme.breakpoints.up("sm")]: {
+    //     width: `calc(100% - ${drawerWidth}px)`,
+    //     marginLeft: drawerWidth,
+    // },
+  },
 
-    toolbar: {
-        // display: "flex",
-        alignItems: "center",
-        // justifyContent: "flex-end",
-        paddingTop: theme.spacing(3),
-        padding: theme.spacing(0, 1),
-        position: "relative",
-        // necessary for content to be below app bar
-        ...theme.mixins.toolbar,
-    },
-    container: {
-        paddingTop: theme.spacing(3),
-        paddingBottom: theme.spacing(4),
-    },
+  toolbar: {
+    // display: "flex",
+    alignItems: 'center',
+    // justifyContent: "flex-end",
+    paddingTop: theme.spacing(3),
+    padding: theme.spacing(0, 1),
+    position: 'relative',
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  },
+  container: {
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(4),
+  },
 }));
 
 function Content({ children }) {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <main className={classes.content}>
-            <Container maxWidth="xl" className={classes.toolbar}>
-                {children}
-            </Container>
-        </main>
-    );
+  return (
+    <main className={classes.content}>
+      <Container maxWidth="xl" className={classes.toolbar}>
+        {children}
+      </Container>
+    </main>
+  );
 }
 
 export default Content;
