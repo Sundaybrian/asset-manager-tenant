780ad62499f18f4b8271227423854d536e6a736a
Brian Sunday <brians931@gmail.com>
2021-08-25 14:41:54 +0300

    add: poor typings for store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,24 +1,16 @@
-import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk";
-import rootReducer from "./Reducers/rootReducer";
-import { getFirebase } from "react-redux-firebase";
-import fbconfig from "../Utils/fb.config";
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from './Reducers/rootReducer';
+import { getFirebase } from 'react-redux-firebase';
 
 const initialState = {};
 
 const middleware = [thunk.withExtraArgument({ getFirebase })];
 
-const store = createStore(
-    rootReducer,
-    initialState,
-    composeWithDevTools(applyMiddleware(...middleware))
-);
-
-export const rrfProps = {
-    firebase: fbconfig,
-    config: {},
-    dispatch: store.dispatch,
-};
+const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
 
 export default store;
+
+// create your own dispatch function reference with custom typings
+export const dispatchStore = store.dispatch as typeof store.dispatch | any;
