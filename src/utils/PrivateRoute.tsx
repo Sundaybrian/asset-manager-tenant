68ae682de14d2abc486f2184a3169d9b70266bd7
Brian Sunday <brians931@gmail.com>
2021-08-25 17:01:15 +0300

    add: private route component

--- src/utils/PrivateRoute.tsx
+++ src/utils/PrivateRoute.tsx
@@ -1,21 +1,14 @@
-import React from "react";
-import { Route, Redirect } from "react-router-dom";
-import { connect } from "react-redux";
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 const PrivateRoute = ({ component: Component, user, auth, ...rest }) => {
-    return (
-        <Route
-            {...rest}
-            render={(props) =>
-                !auth.uid ? (<Redirect to="/login" />) : <Component {...props} />
-            }
-        />
-    );
+  return <Route {...rest} render={(props) => (!auth.uid ? <Redirect to="/login" /> : <Component {...props} />)} />;
 };
 
 const mapStateToProps = (state) => ({
-    user: state.auth.user,
-    auth: state.firebase.auth,
+  user: state.auth.user,
+  auth: state.firebase.auth,
 });
 
 export default connect(mapStateToProps)(PrivateRoute);
