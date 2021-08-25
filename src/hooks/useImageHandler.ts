448870614b68b00ecca6cd4b46a9bfd60d087e15
Brian Sunday <brians931@gmail.com>
2021-08-25 17:00:35 +0300

    update image handler to accept different files

--- src/hooks/useImageHandler.ts
+++ src/hooks/useImageHandler.ts
@@ -1,18 +1,35 @@
 import React from "react";
-
-const useImageHandler = () => {
+import { setSuccess, setErrors } from "../redux/Actions/alertActions";
+// expand for docs
+const useImageHandler = ({ accept }) => {
     const [file, setFile] = React.useState(null);
     const [imageUrl, setImageUrl] = React.useState(null); //once we have an image, we will duplicate it here
-    const types = ["image/png", "image/jpeg", "image/jpg"];
+    const types =
+        accept == true
+            ? [
+                  "application/pdf",
+                  "application/doc",
+                  "application/docx",
+                  ".doc",
+                  ".docx",
+                  "application/msword",
+                  "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+              ]
+            : ["image/png", "image/jpeg", "image/jpg"];
+
     const imageChangeHandler = (e) => {
         const selected = e.target.files[0];
         if (selected && types.includes(selected.type)) {
             setFile(selected);
             console.log(selected);
+            setSuccess("file set successfully");
 
             // clearError
         } else {
             setFile(null);
+            accept !== undefined
+                ? setErrors("please select a doc or pdf or jpg file")
+                : setErrors("please select a file with a jpg or jpeg format");
             // setALertError e,g select an image file (png or jpeg)
         }
     };
