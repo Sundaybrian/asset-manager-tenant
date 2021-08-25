9e85d7c2b87394fac543409e08f27be8a879806c
Brian Sunday <brians931@gmail.com>
2021-08-25 19:47:09 +0300

    add: more components

--- src/components/Base/People/PeopleDialog.js
+++ src/components/Base/People/PeopleDialog.js
@@ -1,54 +1,41 @@
-import React from "react";
-import Dialog from "@material-ui/core/Dialog";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogTitle from "@material-ui/core/DialogTitle";
+import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
-export default function PeopleDialog({
-    title,
-    data,
-    render,
-    onSave,
-    children,
-}) {
-    const [open, setOpen] = React.useState(false);
+export default function PeopleDialog({ title, data, render, onSave, children }) {
+  const [open, setOpen] = React.useState(false);
 
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    // const handleSave = () => {
-    //     // const action = data ? update : add;
-    //     // dispatch(action({ name, id: id || nextID(), img }));
-    //     onSave && onSave();
-    //     handleClose();
-    // };
+  // const handleSave = () => {
+  //     // const action = data ? update : add;
+  //     // dispatch(action({ name, id: id || nextID(), img }));
+  //     onSave && onSave();
+  //     handleClose();
+  // };
 
-    return (
-        <>
-            {render(handleClickOpen)}
-            <Dialog
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="form-dialog-title"
-                maxWidth="md"
-            >
-                <DialogTitle id="form-dialog-title">{title}</DialogTitle>
-                <DialogContent dividers>
-                    {React.cloneElement(children, { handleClose })}
-                </DialogContent>
-                {/* <DialogActions>
+  return (
+    <>
+      {render(handleClickOpen)}
+      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title" maxWidth="md">
+        <DialogTitle id="form-dialog-title">{title}</DialogTitle>
+        <DialogContent dividers>{React.cloneElement(children, { handleClose })}</DialogContent>
+        {/* <DialogActions>
                     <Button onClick={handleClose} color="primary">
                         {data ? "Close" : "Cancel"}
                     </Button> */}
-                {/* <Button onClick={handleSave} color="primary">
+        {/* <Button onClick={handleSave} color="primary">
                         Save
                     </Button> */}
-                {/* </DialogActions> */}
-            </Dialog>
-        </>
-    );
+        {/* </DialogActions> */}
+      </Dialog>
+    </>
+  );
 }
