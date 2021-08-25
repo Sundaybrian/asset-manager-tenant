3620b4dd5075918757f0bec9416e33be1255663e
Brian Sunday <brians931@gmail.com>
2021-08-25 14:17:58 +0300

    update: remove key prop

--- src/utils/RouteWithSubRoutes.tsx
+++ src/utils/RouteWithSubRoutes.tsx
@@ -1,19 +1,10 @@
-import React from "react";
-import { Route, withRouter } from "react-router-dom";
+import React from 'react';
+import { Route, withRouter } from 'react-router-dom';
 
 function RouteWithSubRoutes(route) {
-    return (
-        <Route
-            path={route.path}
-            render={(props) => (
-                <route.component
-                    {...props}
-                    routes={route.routes}
-                    key={props.key}
-                />
-            )}
-        />
-    );
+  return (
+    <Route path={route.path} render={(props) => <route.component {...props} routes={route.routes}  />} />
+  );
 }
 
 export default withRouter(RouteWithSubRoutes);
