d4de0560e55705bc7be73e5bcff14b169e3dd0b9
Brian Sunday <brians931@gmail.com>
2021-08-25 17:02:53 +0300

    add: title component

--- src/components/Base/Title.js
+++ src/components/Base/Title.js
@@ -1,21 +1,16 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Typography from "@material-ui/core/Typography";
+import React from 'react';
+import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
 
 export default function Title(props) {
-    const { color, variant } = props;
-    return (
-        <Typography
-            component="h2"
-            variant={variant || "h6"}
-            color={color || "primary"}
-            gutterBottom
-        >
-            {props.children}
-        </Typography>
-    );
+  const { color, variant } = props;
+  return (
+    <Typography component="h2" variant={variant || 'h6'} color={color || 'primary'} gutterBottom>
+      {props.children}
+    </Typography>
+  );
 }
 
 Title.propTypes = {
-    children: PropTypes.node,
+  children: PropTypes.node,
 };
