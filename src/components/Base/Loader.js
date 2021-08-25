10f5e770a08961d520fa617abd193b7f799100df
Brian Sunday <brians931@gmail.com>
2021-08-25 15:59:47 +0300

    add: loader component

--- src/components/Base/Loader.js
+++ src/components/Base/Loader.js
@@ -1,17 +1,17 @@
-import { CircularProgress, Grid } from "@material-ui/core";
-import React from "react";
-import Content from "../Container/Content";
+import { CircularProgress, Grid } from '@material-ui/core';
+import React from 'react';
+import Content from '../Container/Content';
 
 function Loader() {
-    return (
-        <Content>
-            <Grid container justify="center">
-                <Grid item>
-                    <CircularProgress />
-                </Grid>
-            </Grid>
-        </Content>
-    );
+  return (
+    <Content>
+      <Grid container justify="center">
+        <Grid item>
+          <CircularProgress />
+        </Grid>
+      </Grid>
+    </Content>
+  );
 }
 
 export default Loader;
